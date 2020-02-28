a3346a4c64257c1ef15a7c0e6e9c32c8c49f2153
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2020-02-28 15:56:33 -0500

    familiarization study complete

---
+++ vignettes.js
@@ -0,0 +1,220 @@
+var Heinz = {
+    "immoral": {
+        13: 'steal the money from his work',
+        14: 'blackmail someone for the money',
+        15: 'commit credit card fraud',
+        16: 'get the medicine by stealing'
+    },
+    "irrational": {
+        21: 'sell his own medication',
+        22: 'sell all his clothes for money',
+        23: 'go on strike until wages increase',
+        24: 'try random other medicines'
+    },
+    "improbable": {
+        17: 'be given the medicine by a doctor',
+        18: 'inherit money from a close relative',
+        19: 'receive the money from a stranger',
+        20: 'discover a cheap new medicine'
+    },
+    "possible": {
+        1: 'sells some of his stocks or bonds',
+        2: 'use a generic medication instead',
+        3: 'buy a different insurance plan',
+        4: 'enroll in federal healthcare',
+        5: 'borrow the money from a bank',
+        6: 'be given the money by a friend',
+        7: 'loan the money from his family',
+        8: 'sell an extra car for the money',
+    },
+    "impossible": {
+        9: 'be given the money by a turtle',
+        10: 'make the money time traveling',
+        11: 'grow the money on a tree',
+        12: 'get the money by clapping'
+    }
+}
+
+var Josh = {
+    "immoral": {
+        37: "steal another person's car",
+        38: 'take a taxi without paying',
+        39: 'take another driver hostage',
+        40: 'sneak onto public transportation'
+    },
+    "irrational": {
+        45: 'book a flight for next year',
+        46: 'sell car for ride to airport',
+        47: 'pay 1000 dollars for a ride',
+        48: 'try to run there in time'
+    },
+    "improbable": {
+        41: 'beckon a friend passing by',
+        42: 'convince airport to delay flight',
+        43: 'get a stranger to drive him',
+        44: 'arrange for a private plane'
+    },
+    "possible": {
+        25: 'reschedule for a later flight',
+        26: 'ask the gas station clerk for help	',
+        27: 'take the public bus there',
+        28: 'request aiport shuttle pickup',
+        29: 'ask a relative to pick him up',
+        30: 'hail a taxi at the intersection',
+        31: 'call a friend for a ride',
+        32: 'take public transportation',
+    },
+    "impossible": {
+        33: 'get a new car by thinking',
+        34: 'ride a cat to the airport',
+        35: 'teleport himself to the airport',
+        36: 'levitate and fly to the airport'
+    }
+}
+
+var Brian = {
+    "immoral": {
+        61: 'overcharge the next customers',
+        62: 'steal money from a coworker',
+        63: 'lie about how much the bakery sold',
+        64: 'give new customers incorrect change'
+    },
+    "irrational": {
+        69: 'close the store five hours early',
+        70: 'pay with his retirement fund',
+        71: 'try to invent new pastries',
+        72: 'discount food to encourage tipping'
+    },
+    "improbable": {
+        65: 'find the money on the ground',
+        66: 'discover the money is in his pocket',
+        67: 'get a child to donate the money',
+        68: 'receive a 100 dollar tip'
+    },
+    "possible": {
+        49: 'explain the situation to his boss',
+        50: 'ask other employees what to do',
+        51: 'get a payday advance loan',
+        52: 'sell something he owns for money',
+        53: 'use money from his own wallet',
+        54: 'add in money from his daily tips',
+        55: 'borrow money from a friend',
+        56: 'write a check for $50.75',
+    },
+    "impossible": {
+        57: 'turn the cash register invisible',
+        58: 'use some monopoly money instead',
+        59: 'take pictures of missing bills',
+        60: 'get the money by sneezing a lot'
+    }
+}
+var Josh = {
+    "immoral": {
+        85: "take someone else's gym card",
+        86: 'claim she left her wallet inside',
+        87: 'sneak past the gym attendant',
+        88: 'go in the back door without paying'
+    },
+    "irrational": {
+        93: 'decide to never exercise again',
+        94: 'try again in three minutes',
+        95: 'complain in a foreign language',
+        96: 'run back and forth across lobby'
+    },
+    "improbable": {
+        89: 'learn the gym stopped checking IDs',
+        90: 'receive a free lifetime membership',
+        91: 'find a one-day pass in purse',
+        92: 'discover the gym is free today'
+    },
+    "possible": {
+        73: 'wait to go to the gym tomorrow',
+        74: 'go for a run outside instead',
+        75: 'decide to lift weights at home',
+        76: 'eat lunch instead of working out	',
+        77: "use a friend's guest pass",
+        78: 'pay for a one-day gym pass',
+        79: 'be allowed in just this once',
+        80: 'renew her gym membership',
+    },
+    "impossible": {
+        81: 'turn invisible and go in',
+        82: 'wish all gyms to be free forever',
+        83: 'renew her membership by blinking',
+        84: 'turn her hat into a gym pass'
+    }
+}
+
+var Mary = {
+    "immoral": {
+        109: "quickly copy someone's homework",
+        110: "put her name on a friend's homework",
+        111: 'forge an excuse note',
+        112: 'lie about being sick to go home'
+    },
+    "irrational": {
+        117: 'ask her mom to FedEx the homework',
+        118: 'look under the lunchroom tables',
+        119: 'turn in a completely blank page',
+        120: "turn in last week's homework again"
+    },
+    "improbable": {
+        113: 'discover her teacher is absent',
+        114: 'learn school is canceled today',
+        115: 'remember she has an extra copy',
+        216: 'be given an A by mistake'
+    },
+    "possible": {
+        97: 'run home to get her homework',
+        98: 'bring in her homework tomorrow',
+        99: 'ask for an extra-credit assignmen',
+        100: 'ask her mom what she should do',
+        101: 'explain what happened to her teacher',
+        102: 'quickly redo the homework assignment',
+        103: 'ask her mom to bring the homework',
+        104: 'have her parents call the teacher',
+    },
+    "impossible": {
+        105: 'have her homework teleported',
+        106: 'remind herself yesterday evening',
+        107: 'turn in several socks instead',
+        108: "erase the teacher's memory"
+    }
+}
+
+var Brad = {
+    "immoral": {
+        133: "secretly take his friends' food",
+        134: 'leave the weakest people behind',
+        135: 'rob other hikers they meet',
+        136: "kill his friend's pet dog for food"
+    },
+    "irrational": {
+        141: 'go hunting for water buffalo',
+        142: 'eat random berries and leaves',
+        143: 'each venture off independently',
+        144: 'eat all their food immediately'
+    },
+    "improbable": {
+        137: 'find thermal vent to keep warm',
+        138: 'find stream showing the path out',
+        139: 'meet a former expedition guide',
+        140: 'find an abandoned bear cave'
+    },
+    "possible": {
+        121: 'stay put and wait for help',
+        122: 'follow a river out of the wilderness',
+        123: 'alert rescuers with emergency flares',
+        124: 'find a river for fresh water and fish',
+        125: 'get more food by hunting and fishing',
+        126: 'build a large fire to alert rescuers',
+        127: 'create a shelter to stay warm',
+        128: 'be found by the rescue crew',
+    },
+    "impossible": {
+        129: 'use his mind to keep everyone warm',
+        130: 'make new jackets from pine trees',
+        131: 'alert airplanes overhead by yelling',
+        132: 'grind up rocks for food and water'
+    }
+}
